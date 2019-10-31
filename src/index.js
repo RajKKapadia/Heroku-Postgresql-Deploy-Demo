624aef44d50bb67ad6d388c8aba231eef7ec8250
RajKKapadia <raajforyou@gmail.com>
2019-10-31 11:44:19 +0530

    Added port

--- src/index.js
+++ src/index.js
@@ -58,6 +58,6 @@ app.post('/add-user', async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server is listening on port 5000.');
 });
